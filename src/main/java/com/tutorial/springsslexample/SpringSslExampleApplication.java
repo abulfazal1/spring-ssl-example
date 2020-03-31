eee42ed7e3c4fd50353a053dbcc903338caeda20
212590189 <abulfazal.laskar@ge.com>
2020-03-31 16:07:03 +0530

    initia

---
+++ src/main/java/com/tutorial/springsslexample/SpringSslExampleApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.springsslexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSslExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSslExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorial/springsslexample/SpringSslExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.springsslexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSslExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
